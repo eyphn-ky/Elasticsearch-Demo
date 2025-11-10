f7500e98aebeb4438fcb02b7c46c0370b0ad7848
EYUPHAN\eyuph <eyuphan.kaya34@gmail.com>
2025-11-11 00:40:27 +0300

    Crud işlemleri yazıldı.

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Elasticsearch.API.Models;
+﻿using Elasticsearch.API.DTOs;
+using Elasticsearch.API.Models;
 
 using Nest;
 
@@ -7,6 +8,7 @@ namespace Elasticsearch.API.Repository
     public class ProductRepository
     {
         private readonly ElasticClient _client;
+        private const string indexName="products";
         public ProductRepository(ElasticClient client)
         {
             _client=client;
@@ -15,13 +17,45 @@ namespace Elasticsearch.API.Repository
         public async Task<Product?> SaveAsync(Product newProduct)
         {
             newProduct.Created=DateTime.Now;
-            var response = await _client.IndexAsync(newProduct,x=>x.Index("products").Id(Guid.NewGuid().ToString()));
+            //index metodu ile veriyi ekliyoruz Elastic tarafında indexlemek kaydetmek demektir.
+            var response = await _client.IndexAsync(newProduct,x=>x.Index(indexName).Id(Guid.NewGuid().ToString()));
             
             //fast fail yöntemi mümkün olduğunca else'lerden kurtul.
             if(!response.IsValid) return null;
 
             newProduct.Id=response.Id;
             return newProduct;
+        }
+
+        public async Task<IReadOnlyCollection<Product>> GetAllAsync()
+        {
+            var result = await _client.SearchAsync<Product>(s=>s.Index(indexName).Query(q=>q.MatchAll()));
+            //ürün id'si source dışında hit içinde geliyor burada source'a setledik.
+            foreach (var hit in result.Hits)
+            {
+                hit.Source.Id = hit.Id;
+            }
+            return result.Documents;
+        }
+
+        public async Task<Product?> GetByIdAsync(string Id)
+        {
+            var response = await _client.GetAsync<Product>(Id, x => x.Index(indexName));
+            if (!response.IsValid) { return null;}
+            response.Source.Id = response.Id;
+            return response.Source;
+        }
+
+        public async Task<bool> UpdateAsync(ProductUpdateDto updateProduct)
+        {
+            var response = await _client.UpdateAsync<Product, ProductUpdateDto>(updateProduct.Id, u => u.Index(indexName).Doc(updateProduct));
+            return response.IsValid;
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            var response = await _client.DeleteAsync<Product>(id, x => x.Index(indexName));
+            return response.IsValid;
         }
     }
 }
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,6 +1,6 @@
 ﻿using Elasticsearch.API.DTOs;
 using Elasticsearch.API.Repository;
-
+using System.Collections.Immutable;
 using System.Net;
 
 namespace Elasticsearch.API.Services
@@ -16,19 +16,70 @@ namespace Elasticsearch.API.Services
         public async Task<ResponseDto<ProductDto>> SaveAsync(ProductCreateDto request)
         {
             var responseProduct = await _productrepository.SaveAsync(request.CreateProduct());
-            if(responseProduct  == null) {
+            if (responseProduct == null)
+            {
                 return ResponseDto<ProductDto>.Fail(
-                    new List<string> 
-                    { 
-                        "Kayıt esnasında bir hata meydana geldi." 
-                    }, 
-                    HttpStatusCode.InternalServerError 
+                    new List<string>
+                    {
+                        "Kayıt esnasında bir hata meydana geldi."
+                    },
+                    HttpStatusCode.InternalServerError
                 );
             }
             else
             {
                 return ResponseDto<ProductDto>.Success(responseProduct.CreateDto(), HttpStatusCode.Created);
             }
+        }
+
+        public async Task<ResponseDto<List<ProductDto>>> GetAllAsync()
+        {
+            var products = await _productrepository.GetAllAsync();
+            var productListDto = new List<ProductDto>();
+            //var productListDto = products.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Stock, new ProductFeatureDto(p.Feature.Width, p.Feature.Height, p.Feature.Color))).ToList();
+
+            foreach (var x in products)
+            {
+                if (x.Feature is null)
+                {
+                    productListDto.Add(new ProductDto(x.Id, x.Name, x.Price, x.Stock, null));
+                }
+                else
+                {
+                    productListDto.Add(new ProductDto(x.Id, x.Name, x.Price, x.Stock, new ProductFeatureDto(x.Feature.Width, x.Feature.Height, x.Feature.Color.ToString())));
+                }
+            }
+            return ResponseDto<List<ProductDto>>.Success(productListDto, HttpStatusCode.OK);
+        }
+
+        public async Task<ResponseDto<ProductDto>> GetByIdAsync(string id)
+        {
+            var hasProduct = await _productrepository.GetByIdAsync(id);
+            if (hasProduct == null)
+            {
+                return ResponseDto<ProductDto>.Fail("Ürün bulunamadı.", HttpStatusCode.NotFound);
+            }
+            return ResponseDto<ProductDto>.Success(hasProduct.CreateDto(), HttpStatusCode.OK);
+        }
+        
+        public async Task<ResponseDto<bool>> UpdateAsync(ProductUpdateDto updateProduct)
+        {
+            var isSuccess = await _productrepository.UpdateAsync(updateProduct);
+            if (!isSuccess)
+            {
+                return ResponseDto<bool>.Fail(new List<string> { "Update sırasında bir hata meydana geldi"}, HttpStatusCode.InternalServerError);
+            }
+            return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        }
+
+        public async Task<ResponseDto<bool>> DeleteAsync(string id)
+        {
+            var isSuccess = await _productrepository.DeleteAsync(id);
+            if (!isSuccess)
+            {
+                return ResponseDto<bool>.Fail(new List<string> { "Delete işlemi sırasında bir hata oluştu" }, HttpStatusCode.InternalServerError);
+            }
+            return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
         }
     }
 }
